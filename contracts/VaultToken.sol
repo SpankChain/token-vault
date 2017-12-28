d1d90c2fe5f56a2ff4056755f77d3944e101dcb3
nginnever <ginneversource@gmail.com>
2017-12-28 10:19:53 -0800

    fix setVaultAddress

--- contracts/VaultToken.sol
+++ contracts/VaultToken.sol
@@ -1,16 +1,3 @@
-/*
-This Token Contract implements the standard token functionality (https://github.com/ethereum/EIPs/issues/20) as well as the following OPTIONAL extras intended for use by humans.
-
-In other words. This is intended for deployment in something like a Token Factory or Mist wallet, and then used by humans.
-Imagine coins, currencies, shares, voting weight, etc.
-Machine-based, rapid creation of many tokens would not necessarily need these extra features or will be minted in other manners.
-
-1) Initial Finite Supply (upon creation one specifies how much is minted).
-2) In the absence of a token registry: Optional Decimal, Symbol & Name.
-3) Optional approveAndCall() functionality to notify a contract if an approval() has occurred.
-
-.*/
-
 import "./StandardToken.sol";
 import "./HumanStandardToken.sol";
 import "./TokenVault.sol";
@@ -55,10 +42,11 @@ contract VaultToken is StandardToken {
     }
 
     function setVaultAddress(address _tokenVault) returns(bool success) {
+        // only once
+        require(vaultSet == false);
         require(_tokenVault != 0x0);
         vault = TokenVault(_tokenVault);
         require(vault.balances(this) == totalSupply);
-        // only once
         vaultSet = true;
     }
 
